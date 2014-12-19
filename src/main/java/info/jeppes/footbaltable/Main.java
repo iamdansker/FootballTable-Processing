856f0a2aec7a2fb224bbe5668f88b2d86274a541
Mathias Immerkær <maimm10@student.sdu.dk>
2014-12-19 10:03:42 +0100

    Changed LookAndFeel

--- src/main/java/info/jeppes/footbaltable/Main.java
+++ src/main/java/info/jeppes/footbaltable/Main.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package info.jeppes.footbaltable;
 
 import info.jeppes.footbaltable.graphs.DisplayGeneralStatistics;
@@ -15,62 +14,63 @@ import info.jeppes.footbaltable.graphs.DisplayMatchesPerWeekday;
 import info.jeppes.footbaltable.graphs.DisplayWinRate;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import processing.core.PApplet;
 
 /**
  *
  * @author jeppe
  */
-public class Main extends JFrame{
-     public Main() {
-         super("Embedded PApplet");
+public class Main extends JFrame {
 
-         setLayout(new BorderLayout());
-         DisplayMatchTable displayMatchTable = new DisplayMatchTable();
-         add(displayMatchTable, BorderLayout.CENTER);
-         DisplayGeneralStatistics displayGeneralStatistics = new DisplayGeneralStatistics();
-         add(displayGeneralStatistics, BorderLayout.EAST);
-         
-         JPanel northPanel = new JPanel();
-         northPanel.setLayout(new GridLayout(0, 3));
-         add(northPanel,BorderLayout.NORTH);
-         
-         
-         PApplet goalsPerWeekDay = new DisplayGoalsPerWeekDay();
-         goalsPerWeekDay.init();
-         
-         JPanel jPanel = new JPanel();
-         jPanel.setLayout(new BorderLayout());
-         
-         PApplet winRate = new DisplayWinRate();
-         winRate.init();
-         jPanel.add(winRate, BorderLayout.NORTH);
-         
-         PApplet goalsPerPlayer = new DisplayGoalsPerPlayer();
-         goalsPerPlayer.init();
-         jPanel.add(goalsPerPlayer, BorderLayout.SOUTH);
-         System.out.println("test: "+jPanel.getPreferredSize());
-         
-         PApplet matchesPerWeekday = new DisplayMatchesPerWeekday();
-         matchesPerWeekday.init();
-         
-         PApplet matchDuritations = new DisplayMatchDuritations();
-         matchDuritations.init();
-         
-         northPanel.add(goalsPerWeekDay);
-         northPanel.add(matchesPerWeekday);
+    public Main() {
+        super("Embedded PApplet");
+
+        setLayout(new BorderLayout());
+        DisplayMatchTable displayMatchTable = new DisplayMatchTable();
+        add(displayMatchTable, BorderLayout.CENTER);
+        DisplayGeneralStatistics displayGeneralStatistics = new DisplayGeneralStatistics();
+        add(displayGeneralStatistics, BorderLayout.EAST);
+
+        JPanel northPanel = new JPanel();
+        northPanel.setLayout(new GridLayout(0, 3));
+        add(northPanel, BorderLayout.NORTH);
+
+        PApplet goalsPerWeekDay = new DisplayGoalsPerWeekDay();
+        goalsPerWeekDay.init();
+
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new BorderLayout());
+
+        PApplet winRate = new DisplayWinRate();
+        winRate.init();
+        jPanel.add(winRate, BorderLayout.NORTH);
+
+        PApplet goalsPerPlayer = new DisplayGoalsPerPlayer();
+        goalsPerPlayer.init();
+        jPanel.add(goalsPerPlayer, BorderLayout.SOUTH);
+        System.out.println("test: " + jPanel.getPreferredSize());
+
+        PApplet matchesPerWeekday = new DisplayMatchesPerWeekday();
+        matchesPerWeekday.init();
+
+        PApplet matchDuritations = new DisplayMatchDuritations();
+        matchDuritations.init();
+
+        northPanel.add(goalsPerWeekDay);
+        northPanel.add(matchesPerWeekday);
 //         northPanel.add(jPanel);
-         northPanel.add(matchDuritations);
-         add(jPanel, BorderLayout.WEST);
-         
-         
-         
+        northPanel.add(matchDuritations);
+        add(jPanel, BorderLayout.WEST);
+
 //         PApplet goalsPerWeekDay = new DisplayGoalsPerWeekDay();
 //         goalsPerWeekDay.init();
 //         add(goalsPerWeekDay,BorderLayout.WEST);
-         
 //         PApplet embed = new DisplayGoalsOverTime();
 //         embed.init();
 //         add(embed, BorderLayout.WEST);
@@ -82,7 +82,6 @@ public class Main extends JFrame{
 //         PApplet embed3 = new DisplayWinRate();
 //         embed3.init();
 //         add(embed3, BorderLayout.SOUTH);
-         
 //         PApplet embed4 = new DisplayMatchesPerWeekday();
 //         embed4.init();
 //         add(embed4);
@@ -90,16 +89,22 @@ public class Main extends JFrame{
 //         PApplet embed5 = new DisplayGoalsPerPlayer();
 //         embed5.init();
 //         add(embed5);
-         
-         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         this.pack();
-         this.setVisible(true);
-         System.out.println(displayMatchTable.getPreferredSize());
-     }
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        System.out.println(displayMatchTable.getPreferredSize());
+    }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         new Main();
 //        AppletViewer viewer = AppletLauncher.applet(FootballTableGUI.class).start();
-        
+
     }
 }
